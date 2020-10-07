48cdf8ec5e4d91b4e298fae850a13929ab93e590
James-Jeong <ehddnr177@naver.com>
2020-10-07 02:14:46 -0400

    주석 추가

--- src/javltree.c
+++ src/javltree.c
@@ -5,23 +5,33 @@
 #include "../include/javltree.h"
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Predefinition of Static Functions
+/// Predefinition of JNode Static Functions
 ////////////////////////////////////////////////////////////////////////////////
 
-static KeyType JAVLTreeCheckKeyType(KeyType type);
-static JNodePtr JAVLTreeRotateLL(JNodePtr node);
-static JNodePtr JAVLTreeRotateLR(JNodePtr node);
-static JNodePtr JAVLTreeRotateRR(JNodePtr node);
-static JNodePtr JAVLTreeRotateRL(JNodePtr node);
+static JNodePtr JNodeRotateLL(const JNodePtr node);
+static JNodePtr JNodeRotateLR(const JNodePtr node);
+static JNodePtr JNodeRotateRR(const JNodePtr node);
+static JNodePtr JNodeRotateRL(const JNodePtr node);
+static int JNodeGetHeight(const JNodePtr node);
+static int JNodeGetHeightDiff(const JNodePtr node);
+static void JNodeDeleteChilds(JNodePtr node);
+static JNodePtr JNodeMove(JNodePtr node, void *key, KeyType type);
+static void JNodePreorderTraverse(const JNodePtr node, KeyType type);
+static void JNodeInorderTraverse(const JNodePtr node, KeyType type);
+static void JNodePostorderTraverse(const JNodePtr node, KeyType type);
+static void JNodePrintKey(const JNodePtr node, KeyType type);
+
+////////////////////////////////////////////////////////////////////////////////
+/// Predefinition of JAVLTree Static Function
+////////////////////////////////////////////////////////////////////////////////
+
 static JAVLTreePtr JAVLTreeRebalance(JAVLTreePtr tree);
-static int JAVLTreeGetHeight(JNodePtr node);
-static int JAVLTreeGetHeightDiff(JNodePtr node);
-static void JAVLTreeDeleteNodes(JNodePtr node);
-static JNodePtr JAVLTreeMoveNode(JNodePtr node, void *key, KeyType type);
-static void _PreorderTraverse(JNodePtr node, KeyType type);
-static void _InorderTraverse(JNodePtr node, KeyType type);
-static void _PostorderTraverse(JNodePtr node, KeyType type);
-static void _PrintKey(JNodePtr node, KeyType type);
+
+////////////////////////////////////////////////////////////////////////////////
+/// Predefinition of Util Static Functions
+////////////////////////////////////////////////////////////////////////////////
+
+static KeyType _CheckKeyType(KeyType type);
 static int _GetCompareLength(const char *s1, const char *s2);
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -65,9 +75,9 @@ DeleteResult DeleteJNode(JNodePtrContainer container)
 
 /**
  * @fn void* JNodeGetKey(const JNodePtr node)
- * @brief 노드에 저장된 데이터의 주소를 반환하는 함수
+ * @brief 노드에 저장된 키의 주소를 반환하는 함수
  * @param node 노드 구조체 객체의 주소(입력, 읽기 전용)
- * @return 성공 시 저장된 데이터의 주소, 실패 시 NULL 반환
+ * @return 성공 시 저장된 키의 주소, 실패 시 NULL 반환
  */
 void* JNodeGetKey(const JNodePtr node)
 {
@@ -77,10 +87,10 @@ void* JNodeGetKey(const JNodePtr node)
 
 /**
  * @fn void* JNodeSetKey(JNodePtr node, void *key)
- * @brief 노드에 데이터의 주소를 저장하는 함수
+ * @brief 노드에 의 주소를 저장하는 함수
  * @param node 노드 구조체 객체의 주소(출력)
- * @param key 저장할 데이터의 주소(입력)
- * @return 성공 시 저장된 데이터의 주소, 실패 시 NULL 반환
+ * @param key 저장할 키의 주소(입력)
+ * @return 성공 시 저장된 키의 주소, 실패 시 NULL 반환
  */
 void* JNodeSetKey(JNodePtr node, void *key)
 {
@@ -101,7 +111,7 @@ void* JNodeSetKey(JNodePtr node, void *key)
  */
 JAVLTreePtr NewJAVLTree(KeyType type)
 {
-	if(JAVLTreeCheckKeyType(type) == Unknown) return NULL;
+	if(_CheckKeyType(type) == Unknown) return NULL;
 
 	JAVLTreePtr newTree = (JAVLTreePtr)malloc(sizeof(JAVLTree));
 	if(newTree == NULL)
@@ -129,7 +139,7 @@ DeleteResult DeleteJAVLTree(JAVLTreePtrContainer container)
 	JNodePtr rootNode = (*container)->root;
 	if(rootNode != NULL)
 	{
-		JAVLTreeDeleteNodes(rootNode);
+		JNodeDeleteChilds(rootNode);
 		free(rootNode);
 	}
 
@@ -140,7 +150,7 @@ DeleteResult DeleteJAVLTree(JAVLTreePtrContainer container)
 }
 
 /**
- * @fn void* JAVLTreeGetKey(const JAVLTreePtr tree)
+ * @fn void* JAVLTreeGetData(const JAVLTreePtr tree)
  * @brief AVL Tree에 저장된 데이터의 주소를 반환하는 함수
  * @param tree AVL Tree 구조체 객체의 주소(입력, 읽기 전용)
  * @return 성공 시 저장된 데이터의 주소, 실패 시 NULL 반환
@@ -170,7 +180,7 @@ void* JAVLTreeSetData(JAVLTreePtr tree, void *data)
  * @brief AVL Tree에 새로운 노드를 추가하는 함수
  * 중복 허용하지 않음
  * @param tree AVL Tree 구조체 객체의 주소(출력)
- * @param key 저장할 노드의 데이터 주소(입력)
+ * @param key 저장할 노드의 키 주소(입력)
  * @return 성공 시 AVL Tree 구조체의 주소, 실패 시 NULL 반환
  */
 JAVLTreePtr JAVLTreeAddNode(JAVLTreePtr tree, void *key)
@@ -185,7 +195,7 @@ JAVLTreePtr JAVLTreeAddNode(JAVLTreePtr tree, void *key)
 		if(key == currentNode->key) return NULL;
 
 		parentNode = currentNode;
-		currentNode = JAVLTreeMoveNode(currentNode, key, tree->type);
+		currentNode = JNodeMove(currentNode, key, tree->type);
 	}
 
 	JNodePtr newNode = NewJNode();
@@ -231,9 +241,9 @@ JAVLTreePtr JAVLTreeAddNode(JAVLTreePtr tree, void *key)
 
 /**
  * @fn DeleteResult JAVLTreeDeleteNodeKey(JAVLTreePtr tree, void *key)
- * @brief AVL Tree에 저장된 데이터를 삭제하는 함수
+ * @brief AVL Tree에 지정한 키를 가진 노드를 삭제하는 함수
  * @param tree AVL Tree 구조체 객체의 주소(츨력)
- * @param key 삭제할 데이터의 주소(입력)
+ * @param key 삭제할 키의 주소(입력)
  * @return 성공 시 DeleteSuccess, 실패 시 DeleteFail 반환(DeleteResult 열거형 참고)
  */
 DeleteResult JAVLTreeDeleteNodeKey(JAVLTreePtr tree, void *key)
@@ -253,7 +263,7 @@ DeleteResult JAVLTreeDeleteNodeKey(JAVLTreePtr tree, void *key)
 	while((currentNode != NULL) && (key != currentNode->key))
 	{
 		parentNode = currentNode;
-		currentNode = JAVLTreeMoveNode(currentNode, key, tree->type);
+		currentNode = JNodeMove(currentNode, key, tree->type);
 	}
 
 	if(currentNode == NULL)
@@ -314,32 +324,56 @@ DeleteResult JAVLTreeDeleteNodeKey(JAVLTreePtr tree, void *key)
 	return result;
 }
 
+/**
+ * @fn void JAVLTreePreorderTraverse(const JAVLTreePtr tree)
+ * @brief AVL Tree 를 전위 순회하며 노드의 키를 출력하는 함수
+ * @param tree 순회할 AVL Tree (입력, 읽기 전용)
+ * @return 반환값 없음
+ */
 void JAVLTreePreorderTraverse(const JAVLTreePtr tree)
 {
 	if(tree == NULL) return;
-	_PreorderTraverse(tree->root, tree->type);
+	JNodePreorderTraverse(tree->root, tree->type);
 	printf("\n");
 }
 
+/**
+ * @fn void JAVLTreeInorderTraverse(const JAVLTreePtr tree)
+ * @brief AVL Tree 를 중위 순회하며 노드의 키를 출력하는 함수
+ * @param tree 순회할 AVL Tree (입력, 읽기 전용)
+ * @return 반환값 없음
+ */
 void JAVLTreeInorderTraverse(const JAVLTreePtr tree)
 {
 	if(tree == NULL) return;
-	_InorderTraverse(tree->root, tree->type);
+	JNodeInorderTraverse(tree->root, tree->type);
 	printf("\n");
 }
 
+/**
+ * @fn void JAVLTreePostorderTraverse(const JAVLTreePtr tree)
+ * @brief AVL Tree 를 후위 순회하며 노드의 키를 출력하는 함수
+ * @param tree 순회할 AVL Tree (입력, 읽기 전용)
+ * @return 반환값 없음
+ */
 void JAVLTreePostorderTraverse(const JAVLTreePtr tree)
 {
 	if(tree == NULL) return;
-	_PostorderTraverse(tree->root, tree->type);
+	JNodePostorderTraverse(tree->root, tree->type);
 	printf("\n");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Static Functions
+/// JNode Static Functions
 ////////////////////////////////////////////////////////////////////////////////
 
-static JNodePtr JAVLTreeRotateLL(JNodePtr node)
+/**
+ * @fn static JNodePtr JNodeRotateLL(JNodePtr node)
+ * @brief 지정한 노드를 기준으로 하위 노드들에 대해 LL 인 상황에서 AVL Tree 를 균형있게 회전하는 함수
+ * @param node 회전하기 위한 기준 노드(입력, 읽기 전용) 
+ * @return 성공 시 회전된 기준 노드, 실패 시 NULL 반환
+ */
+static JNodePtr JNodeRotateLL(const JNodePtr node)
 {
 	if(node == NULL) return NULL;
 
@@ -352,18 +386,30 @@ static JNodePtr JAVLTreeRotateLL(JNodePtr node)
 	return currentNode;
 }
 
-static JNodePtr JAVLTreeRotateLR(JNodePtr node)
+/**
+ * @fn static JNodePtr JNodeRotateLR(const JNodePtr node)
+ * @brief 지정한 노드를 기준으로 하위 노드들에 대해 LR 인 상황에서 AVL Tree 를 균형있게 회전하는 함수
+ * @param node 회전하기 위한 기준 노드(입력, 읽기 전용) 
+ * @return 성공 시 회전된 기준 노드, 실패 시 NULL 반환
+ */
+static JNodePtr JNodeRotateLR(const JNodePtr node)
 {
 	if(node == NULL) return NULL;
 
 	JNodePtr parentNode = node;
 	JNodePtr currentNode = parentNode->left;
 
-	parentNode->left = JAVLTreeRotateRR(currentNode);
-	return JAVLTreeRotateLL(parentNode);
+	parentNode->left = JNodeRotateRR(currentNode);
+	return JNodeRotateLL(parentNode);
 }
 
-static JNodePtr JAVLTreeRotateRR(JNodePtr node)
+/**
+ * @fn static JNodePtr JNodeRotateRR(const JNodePtr node)
+ * @brief 지정한 노드를 기준으로 하위 노드들에 대해 RR 인 상황에서 AVL Tree 를 균형있게 회전하는 함수
+ * @param node 회전하기 위한 기준 노드(입력, 읽기 전용) 
+ * @return 성공 시 회전된 기준 노드, 실패 시 NULL 반환
+ */
+static JNodePtr JNodeRotateRR(const JNodePtr node)
 {
 	if(node == NULL) return NULL;
 
@@ -376,89 +422,87 @@ static JNodePtr JAVLTreeRotateRR(JNodePtr node)
 	return currentNode;
 }
 
-static JNodePtr JAVLTreeRotateRL(JNodePtr node)
+/**
+ * @fn static JNodePtr JNodeRotateRL(const JNodePtr node)
+ * @brief 지정한 노드를 기준으로 하위 노드들에 대해 RL 인 상황에서 AVL Tree 를 균형있게 회전하는 함수
+ * @param node 회전하기 위한 기준 노드(입력, 읽기 전용) 
+ * @return 성공 시 회전된 기준 노드, 실패 시 NULL 반환
+ */
+static JNodePtr JNodeRotateRL(const JNodePtr node)
 {
 	if(node == NULL) return NULL;
 
 	JNodePtr parentNode = node;
 	JNodePtr currentNode = parentNode->right;
 
-	parentNode->right = JAVLTreeRotateLL(currentNode);
-	return JAVLTreeRotateRR(parentNode);
+	parentNode->right = JNodeRotateLL(currentNode);
+	return JNodeRotateRR(parentNode);
 }
 
-static JAVLTreePtr JAVLTreeRebalance(JAVLTreePtr tree)
-{
-	if(tree == NULL) return NULL;
-
-	int heightDiff = JAVLTreeGetHeightDiff(tree->root);
-
-	if(heightDiff > 1)
-	{
-		if(JAVLTreeGetHeightDiff(tree->root->left) > 0) tree->root = JAVLTreeRotateLL(tree->root);
-		else tree->root = JAVLTreeRotateLR(tree->root);
-	}
-
-	if(heightDiff < -1)
-	{
-		if(JAVLTreeGetHeightDiff(tree->root->right) < 0) tree->root = JAVLTreeRotateRR(tree->root);
-		else tree->root = JAVLTreeRotateRL(tree->root);
-	}
-
-	return tree;
-}
-
-static KeyType JAVLTreeCheckKeyType(KeyType type)
-{
-	switch(type)
-	{
-		case IntType:
-		case CharType:
-		case StringType:
-			break;
-		default:
-			return Unknown;
-	}
-	return type;
-}
-
-static int JAVLTreeGetHeight(JNodePtr node)
+/**
+ * @fn static int JNodeGetHeight(const JNodePtr node)
+ * @brief AVL Tree 에서 지정한 노드의 높이를 구하는 함수(재귀)
+ * 루트 노드는 1, 이하 자식 노드 레벨(높이)부터 1 씩 증가
+ * @param node 높이를 구하기 위한 노드(입력, 읽기 전용) 
+ * @return 성공 시 0 이상의 높이, 실패 시 0 반환
+ */
+static int JNodeGetHeight(const JNodePtr node)
 {
 	if(node == NULL) return 0;
 
-	int leftHeight = JAVLTreeGetHeight(node->left);
-	int rightHeight = JAVLTreeGetHeight(node->right);
+	int leftHeight = JNodeGetHeight(node->left);
+	int rightHeight = JNodeGetHeight(node->right);
 
 	if(leftHeight > rightHeight) return leftHeight + 1;
 	else return rightHeight + 1;
 }
 
-static int JAVLTreeGetHeightDiff(JNodePtr node)
+/**
+ * @fn static int JNodeGetHeightDiff(const JNodePtr node)
+ * @brief AVL Tree 에서 지정한 노드의 자식노드들의 높이 차이를 구하는 함수(재귀)
+ * @param node 높이의 차이를 구하기 위한 기준 노드(입력, 읽기 전용) 
+ * @return 성공 시 자식 노드들의 높이 차이, 실패 시 0 반환
+ */
+static int JNodeGetHeightDiff(const JNodePtr node)
 {
 	if(node == NULL) return 0;
-	return JAVLTreeGetHeight(node->left) - JAVLTreeGetHeight(node->right);
+	return JNodeGetHeight(node->left) - JNodeGetHeight(node->right);
 }
 
-static void JAVLTreeDeleteNodes(JNodePtr node)
+/**
+ * @fn static void JNodeDeleteChilds(JNodePtr node)
+ * @brief AVL Tree 에 저장된 노드들을 모두 삭제하는 함수(재귀)
+ * @param node 자식 노드들을 삭제하기 위한 기준 노드(입력)
+ * @return 반환값 없음
+ */
+static void JNodeDeleteChilds(JNodePtr node)
 {
 	if(node == NULL) return;
 	
 	if(node->left != NULL)
 	{
-		JAVLTreeDeleteNodes(node->left);
+		JNodeDeleteChilds(node->left);
 		free(node->left);
 		node->left = NULL;
 	}
 
 	if(node->right != NULL)
 	{
-		JAVLTreeDeleteNodes(node->right);
+		JNodeDeleteChilds(node->right);
 		free(node->right);
 		node->right = NULL;
 	}
 }
 
-static JNodePtr JAVLTreeMoveNode(JNodePtr node, void *key, KeyType type)
+/**
+ * @fn static JNodePtr JNodeMove(JNodePtr node, void *key, KeyType type)
+ * @brief 지정한 노드의 키와 전달받은 키를 비교해서 저장할 노드의 위치를 찾아 그 노드의 주소를 반환하는 함수 
+ * @param node 위치를 움직일 노드의 주소(입력)
+ * @param key 전달받은 키(입력)
+ * @param type 키의 데이터 유형(입력)
+ * @return 성공 시 위치가 변경된 노드의 주소, 실패 시 NULL 반환
+ */
+static JNodePtr JNodeMove(JNodePtr node, void *key, KeyType type)
 {
 	switch(type)
 	{
@@ -481,31 +525,59 @@ static JNodePtr JAVLTreeMoveNode(JNodePtr node, void *key, KeyType type)
 	return node;
 }
 
-static void _PreorderTraverse(JNodePtr node, KeyType type)
+/**
+ * @fn static void JNodePreorderTraverse(const JNodePtr node, KeyType type)
+ * @brief 지정한 노드를 기준으로 전위 순회하며 키를 출력하는 함수(재귀)
+ * @param node 순회할 기준 노드의 주소(입력)
+ * @param type 출력할 키의 데이터 유형(입력)
+ * @return 반환값 없음
+ */
+static void JNodePreorderTraverse(const JNodePtr node, KeyType type)
 {
 	if(node == NULL) return;
-	_PrintKey(node, type);
-	_PreorderTraverse(node->left, type);
-	_PreorderTraverse(node->right, type);
+	JNodePrintKey(node, type);
+	JNodePreorderTraverse(node->left, type);
+	JNodePreorderTraverse(node->right, type);
 }
 
-static void _InorderTraverse(JNodePtr node, KeyType type)
+/**
+ * @fn static void JNodeInorderTraverse(const JNodePtr node, KeyType type)
+ * @brief 지정한 노드를 기준으로 중위 순회하며 키를 출력하는 함수(재귀)
+ * @param node 순회할 기준 노드의 주소(입력)
+ * @param type 출력할 키의 데이터 유형(입력)
+ * @return 반환값 없음
+ */
+static void JNodeInorderTraverse(const JNodePtr node, KeyType type)
 {
 	if(node == NULL) return;
-	_InorderTraverse(node->left, type);
-	_PrintKey(node, type);
-	_InorderTraverse(node->right, type);
+	JNodeInorderTraverse(node->left, type);
+	JNodePrintKey(node, type);
+	JNodeInorderTraverse(node->right, type);
 }
 
-static void _PostorderTraverse(JNodePtr node, KeyType type)
+/**
+ * @fn static void JNodePostorderTraverse(const JNodePtr node, KeyType type)
+ * @brief 지정한 노드를 기준으로 후위 순회하며 키를 출력하는 함수(재귀)
+ * @param node 순회할 기준 노드의 주소(입력)
+ * @param type 출력할 키의 데이터 유형(입력)
+ * @return 반환값 없음
+ */
+static void JNodePostorderTraverse(const JNodePtr node, KeyType type)
 {
 	if(node == NULL) return;
-	_PostorderTraverse(node->left, type);
-	_PostorderTraverse(node->right, type);
-	_PrintKey(node, type);
+	JNodePostorderTraverse(node->left, type);
+	JNodePostorderTraverse(node->right, type);
+	JNodePrintKey(node, type);
 }
 
-static void _PrintKey(JNodePtr node, KeyType type)
+/**
+ * @fn static void JNodePrintKey(const JNodePtr node, KeyType type)
+ * @brief 지정한 노드의 키를 출력하는 함수
+ * @param node 출력할 노드의 주소(입력, 읽기 전용)
+ * @param type 출력할 키의 데이터 유형(입력)
+ * @return 반환값 없음
+ */
+static void JNodePrintKey(const JNodePtr node, KeyType type)
 {
 	switch(type)
 	{
@@ -522,6 +594,68 @@ static void _PrintKey(JNodePtr node, KeyType type)
 	}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// JAVLTree Static Function
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @fn static JAVLTreePtr JAVLTreeRebalance(JAVLTreePtr tree)
+ * @brief AVL Tree 의 높이 균형을 맞추도록 노드들의 위치를 재배열하는 함수
+ * @param tree AVL Tree 의 주소(입력)
+ * @return 성공 시 AVL Tree 의 주소, 실패 시 NULL 반환
+ */
+static JAVLTreePtr JAVLTreeRebalance(JAVLTreePtr tree)
+{
+	if(tree == NULL) return NULL;
+
+	int heightDiff = JNodeGetHeightDiff(tree->root);
+
+	if(heightDiff > 1)
+	{
+		if(JNodeGetHeightDiff(tree->root->left) > 0) tree->root = JNodeRotateLL(tree->root);
+		else tree->root = JNodeRotateLR(tree->root);
+	}
+
+	if(heightDiff < -1)
+	{
+		if(JNodeGetHeightDiff(tree->root->right) < 0) tree->root = JNodeRotateRR(tree->root);
+		else tree->root = JNodeRotateRL(tree->root);
+	}
+
+	return tree;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Util Static Functions
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @fn static KeyType _CheckKeyType(KeyType type)
+ * @brief 지정한 키 데이터 유형이 정의된 유형인지 검사하는 함수
+ * @param type 검사할 키 데이터 유형(입력)
+ * @return 성공 시 전달받은 키 데이터 유형, 실패 시 Unknown 반환(KeyType 열거형 참고)
+ */
+static KeyType _CheckKeyType(KeyType type)
+{
+	switch(type)
+	{
+		case IntType:
+		case CharType:
+		case StringType:
+			break;
+		default:
+			return Unknown;
+	}
+	return type;
+}
+
+/**
+ * @fn static int _GetCompareLength(const char *s1, const char *s2)
+ * @brief 두 문자열을 비교할 때 가장 긴 문자열의 길이를 반환하는 함수
+ * @param s1 첫 번째 비교할 문자열(입력, 읽기 전용)
+ * @param s2 두 번째 비교할 문자열(입력, 읽기 전용)
+ * @return 항상 비교할 길이 반환
+ */
 static int _GetCompareLength(const char *s1, const char *s2)
 {
 	int s1Length = strlen(s1);
